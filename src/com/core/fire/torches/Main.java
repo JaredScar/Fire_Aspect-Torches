eb941506cb01ae5466f6ed51b6cddf5eb3ac68b1
Jack <TheWolfBadger@gmail.com>
2014-07-01 12:19:06 -0400

    When you have a torch out it acts as a light source (Creates fake glow-stone under your feet).

--- src/com/core/fire/torches/Main.java
+++ src/com/core/fire/torches/Main.java
@@ -1,5 +1,6 @@
 package com.core.fire.torches;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
@@ -7,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -35,6 +37,25 @@ public class Main extends JavaPlugin implements Listener {
                 if(p.getItemInHand().getType() == Material.TORCH) {
                     ent.setFireTicks(this.settings.getInt("FireTicks")*20);
                 }
+            }
+        }
+    }
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        Location from = e.getFrom();
+        Location to = e.getTo();
+        Location block = to.subtract(0, 1, 0);
+        if(to.getX() > from.getX() || to.getZ() > from.getZ() || to.getY() > from.getY() || to.getX() < from.getX() || to.getZ() < from.getZ() || to.getY() < from.getY()) {
+            p.sendBlockChange(from.subtract(0, 1, 0), from.getBlock().getType(), from.getBlock().getData());
+            p.sendBlockChange(from, from.getBlock().getType(), from.getBlock().getData());
+            p.sendBlockChange(from, from.getBlock().getType(), from.getBlock().getData());
+        }
+        if(!block.getBlock().getType().equals(Material.AIR) && block.getBlock().getData() == (byte) 0) {
+            if(p.getItemInHand() !=null) {
+               if(p.getItemInHand().getType() == Material.TORCH) {
+                    p.sendBlockChange(block, Material.GLOWSTONE, (byte) 0);
+               }
             }
         }
     }
